a0792d3595b09de822eaa7faf46e8dea9cd0e484
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-06-24 21:43:00 +0530

    Time: 0 ms (100%), Space: 28.4 MB (95.3%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& v) {
         int n=v.size();
-        unordered_map<int,int> m;
-        for (int i=0; i<n; i++){
-            m[v[i]]++;
-            if (m[v[i]]>1) return v[i];
+        for (int i=0; i<n-2; i++){
+            if (v[i]==v[i+1] || v[i]==v[i+2]){
+                return v[i];
+            }
         }
-        return -1;
+        return v.back();
     }
 };
