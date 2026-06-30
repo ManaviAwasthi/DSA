556ee7b71b30f474a2af95f0d496cfe0ae5dc6d6
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-06-30 22:23:49 +0530

    Time: 0 ms (100%), Space: 12.3 MB (63.96%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hIndex(vector<int>& v) {
+        sort(v.begin(),v.end());
+        int n=v.size();
+        int l=0,r=n-1,ans=0;
+        while (l<=r){
+            int mid= l+(r-l)/2;
+            if (v[mid]>=n-mid){
+                ans=n-mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
