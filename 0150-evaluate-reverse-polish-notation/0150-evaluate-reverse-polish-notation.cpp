404450716debdb6c7ceab7260bc884f73e77b8d2
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-07-25 20:55:36 +0530

    Time: 0 ms (100%), Space: 17.1 MB (67.08%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -3,34 +3,29 @@ public:
     int evalRPN(vector<string>& v) {
         int n=v.size();
         stack <int> st;
-        int ans;
         for (int i=0; i<n; i++){
             auto c= v[i];
             if ((c=="+" || c=="-" || c=="*" || c=="/") && st.size()>=2){
                 int second= st.top(); st.pop();
                 int first= st.top(); st.pop();
                 if (c=="+"){
-                    ans=first+second;
+                    st.push(first+second);
                 }
                 else if (c=="-"){
-                    ans=first-second;
+                    st.push(first-second);
                 }
                 else if (c=="*"){
-                    ans=first*second;
+                    st.push(first*second);
                 }
                 else if (c=="/"){
-                    ans=first/second;
+                    st.push(first/second);
                 }
-                st.push(ans);
             }
             else {
                 int m= stoi(c);
                 st.push(m);
             }
         }
-        if (!st.empty()){
-            ans=st.top();
-        }
-        return ans;
+        return st.top();
     }
 };
