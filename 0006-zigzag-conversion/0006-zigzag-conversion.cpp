5aaaf597f357120aee0d450ff8d919bca7f3e75c
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-07-28 11:50:10 +0530

    Time: 17 ms (16.21%), Space: 18.8 MB (8.77%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string convert(string s, int r) {
+        int n=s.length();
+        if (r==1 || r>=n){
+            return s;
+        }
+        vector <vector <char>> rows(n);
+        int pos=0;
+        int d;
+        for (int i=0; i<n;i++){
+            if (pos==0){
+                d=1;
+            }
+            else if (pos==r-1){
+                d=-1;
+            }
+            rows[pos].push_back(s[i]);
+            pos+=d;
+        }
+        string res="";
+        for (auto &it: rows){
+            for (char c: it){
+                res.push_back(c);
+            }
+        }
+        return res;
+        
+    }
+};
