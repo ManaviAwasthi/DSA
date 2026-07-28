4f751f4286f225283bc48ead66bf2fd026e6c5f4
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-07-29 00:55:05 +0530

    Time: 0 ms (100%), Space: 41.8 MB (66.87%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -6,9 +6,12 @@ public:
         int r=0;
         int sum=0;
         int len=INT_MAX;
+
         while (r<n){
             sum+=nums[r];
-            r++;
+            r++; //we move r till we get a sum greater than target
+            //then we move l till we reach less than it
+            //so min dist is r-l
             while (sum>=target){
                 sum-=nums[l];
                 len=min(len,r-l);
