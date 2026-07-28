1cae595821887541b8c695141978431296eb6b7d
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-07-28 10:10:51 +0530

    Time: 26 ms (75.82%), Space: 54.3 MB (88.13%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -3,7 +3,17 @@ public:
     string smallestPalindrome(string s) {
         int n= s.length();
         int mid= n/2;
-        sort (s.begin(), s.begin()+mid);
+        vector <int> freq (26,0);
+        for (int i=0; i<mid; i++){
+            freq[s[i]-'a']++;
+        }
+        int idx=0;
+        for (int i=0; i<26; i++){
+            while (freq[i]--){
+                s[idx]=i+'a';
+                idx++;
+            }
+        }
         for (int i=0; i<mid; i++){
             s[n-i-1]=s[i];
         }
