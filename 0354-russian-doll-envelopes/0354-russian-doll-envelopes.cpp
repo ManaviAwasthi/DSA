a23cdbede8a66821fffabecd18cd14c8b8a4c238
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-08-09 14:54:31 +0530

    Time: 83 ms (19.48%), Space: 92.1 MB (14.96%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& v) {
+        sort (v.begin(),v.end(), [](const vector <int> &a, const vector <int> &b){
+            if (a[0]==b[0]) return a[1]>b[1];
+            return a[0]<b[0];
+        });
+        vector <int> ans;
+        for (auto it: v){
+            int ht= it[1];
+            auto itr= lower_bound(ans.begin(),ans.end(),ht);
+            if (itr==ans.end()) ans.push_back(ht);
+            else *itr=ht;
+        }
+        return ans.size();
+    }
+};
