f6fb62b7247e04a4147f250f1ce3eccefb3f5c07
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-08-11 20:51:18 +0530

    Time: 46 ms (19.34%), Space: 19.4 MB (14.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,19 @@
 class Solution {
-    //Approach: Sliding window and 2 pointers
-
 public:
     int lengthOfLongestSubstring(string s) {
-        vector <int> hash(256,-1); 
-        int n=s.length();
-        int l=0;
-        int r=0;
-        int mlen=0; 
-
-        while (r<n){ //till r reaches end of str
-//If the current character s[r] was seen before, and the last seen index is within the current window (hash[s[r]] >= l), then:Move the left pointer just after the last occurrence to remove the duplicate.This ensures that all characters in the window are unique.
-            if (hash[s[r]]!=-1){
-                if (hash[s[r]]>=l){
-                    l=hash[s[r]]+1;
+        int n= s.size();
+        unordered_map <char, int> m;
+        int l=0, r=0, mlen=0;
+        while (r<n){
+            char c= s[r];
+            if (m.find(c)!=m.end()){
+                if (m[c]>=l){
+                    l=m[c]+1;
                 }
+                
             }
-            mlen=max(mlen,r-l+1); //current window size
-            hash[s[r]]=r; //Record the last seen index of s[r] as r
+            mlen= max(r-l+1,mlen);
+            m[c]=r;
             r++;
         }
         return mlen;
