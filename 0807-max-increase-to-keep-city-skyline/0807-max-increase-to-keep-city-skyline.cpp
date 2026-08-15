9f715430bd980723e6e1a51ce585c6c8915db4f9
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-08-15 19:47:07 +0530

    Time: 0 ms (100%), Space: 13.6 MB (94.93%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.cpp
@@ -2,35 +2,18 @@ class Solution {
 public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& v) {
         int n=v.size();
-        vector <vector <int>> maxi(n, vector <int> (n,0));
-        int mx=0;
+        vector <int> row(n,0);
+        vector <int> col(n,0);
         for (int i=0; i<n; i++){
             for (int j=0; j<n; j++){
-                mx=max(mx,v[i][j]);
+                row [i]=max(row[i], v[i][j]);
+                col [j]=max(col[j], v[i][j]);
             }
-            for (int j=0; j<n; j++){
-                maxi[i][j]=mx;
-            }
-            mx=0;
-        }
-        for (int j=0; j<n; j++){
-            for (int i=0; i<n; i++){
-                mx=max(mx,v[i][j]);
-            }
-            for (int i=0; i<n; i++){
-                maxi[i][j] = min(maxi[i][j], mx);
-            }
-            mx=0;
         }
         int ans=0;
         for (int i=0; i<n; i++){
-            ans+=maxi[i][i]-v[i][i];
-        }
-        for (int i=0; i<n; i++){
             for (int j=0; j<n; j++){
-                if (i!=j){
-                    ans+=maxi[i][j]-v[i][j];
-                }
+                ans+= min(row[i],col[j])-v[i][j];
             }
         }
         return ans;
