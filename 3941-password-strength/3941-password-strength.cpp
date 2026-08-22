e3d8db2fa1cfb92c2f6c66f98626dae9ca6491af
ManaviAwasthi <manaviawasthi8@gmail.com>
2026-08-22 14:48:05 +0530

    Time: 3 ms (94.04%), Space: 16.6 MB (80.64%) - LeetHub

--- 3941-password-strength/3941-password-strength.cpp
+++ 3941-password-strength/3941-password-strength.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int passwordStrength(string password) {
-        unordered_set <char> st;
-        int n=password.length();
-        for (int i=0; i<n; i++){
-            st.insert(password[i]);
+        vector <int> vis(256,0);
+        for (char c: password){
+            vis[c]=1;
         }
         int ans=0;
-        for (char c: st){
-            if (c>='a' && c<='z'){
-                ans++;
-            }
-            else if (c>='A' && c<='Z'){
-                ans+=2;
-            }
-            else if (c>='0' && c<='9'){
-                ans+=3;
-            }
-            else {
-                ans+=5;
+        char c;
+        for (int i=0; i<256; i++){
+            if (vis[i]) {
+                c=i;
+                if (c>='a' && c<='z'){
+                    ans++;
+                }
+                else if (c>='A' && c<='Z'){
+                    ans+=2;
+                }
+                else if (c>='0' && c<='9'){
+                    ans+=3;
+                }
+                else {
+                    ans+=5;
+                }
             }
         }
         return ans;
